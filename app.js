e11716648172cd8b2340bef482002b6e9cd3077f
Abdulnur930 <hussenabdulnur@gmail.com>
2024-03-21 12:01:43 +0300

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT;
 const cors = require("cors");
 app.use(cors());
 //db connection
